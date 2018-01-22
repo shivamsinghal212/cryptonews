061d43d492fd3d71c9ba37b23711f1da6256ad32
shivam <shivam.singhal212@gmail.com>
2018-01-22 17:35:41 +0530

    Added Rest API handle.

--- coinhacker/views.py
+++ coinhacker/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
+def index(request):
+    data = {}
+    data["coin_data"] = get_data()
+    return render(request,"index.html", data)
+
+
+
+def get_data():
+    api_url = "https://api.coinmarketcap.com/v1/ticker/?limit=10"
+
+    try:
+        data = requests.get(api_url).json()
+    except Exception as e:
+        print(e)
+        data = dict()
+
+    return data
