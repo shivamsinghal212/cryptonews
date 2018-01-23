524abd3afc6d1c0ed884ddcb786c6707ff912a58
shivam <shivam.singhal212@gmail.com>
2018-01-23 10:53:53 +0530

    Added template support

--- coinhacker/views.py
+++ coinhacker/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
 import requests
 
+
 # Create your views here.
 def index(request):
     data = {}
     data["coin_data"] = get_data()
-    return render(request,"index.html", data)
+    data["coin_news"] = get_news()['articles']
+    return render(request,"cointable.html", data)
+
+
 
 
 
 def get_data():
-    api_url = "https://api.coinmarketcap.com/v1/ticker/?limit=10"
+    api_url = "https://api.coinmarketcap.com/v1/ticker/?limit=40"
 
     try:
         data = requests.get(api_url).json()
@@ -19,3 +23,13 @@ def get_data():
         data = dict()
 
     return data
+
+
+def get_news():
+    api_url = "https://newsapi.org/v2/top-headlines?sources=crypto-coins-news&apiKey=99fd007d1e724a5791ae6a018caecbc6"
+
+    try:
+        data = requests.get(api_url).json()
+        return data
+    except Exception as e:
+        print(e)
