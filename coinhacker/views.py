0ec5b60ce371dd7f77aee5ba5e6fcce88eac5213
shivam <shivam.singhal212@gmail.com>
2018-01-24 19:11:08 +0530

    Added Top gainers and losers
    Added NavBar

--- coinhacker/views.py
+++ coinhacker/views.py
@@ -7,6 +7,8 @@ def index(request):
     data = {}
     data["coin_data"] = get_data()
     data["coin_news"] = get_news()['articles']
+    data["top_5"] = get_top_5()
+    data["bottom_5"] = get_bottom_5()
     return render(request,"cointable.html", data)
 
 
@@ -33,3 +35,35 @@ def get_news():
         return data
     except Exception as e:
         print(e)
+
+#get top 5 gainers and losers in last 24 hours
+def get_price_all():
+
+    api_url = "https://api.coinmarketcap.com/v1/ticker/?limit=0"
+    name=[]
+    price=[]
+
+    try:
+        data = requests.get(api_url).json()
+        for key in data:
+            for k,v in key.items():
+                if k=='name':
+                    name.append(v)
+                elif k=='percent_change_24h' and v==None:
+                    price.append(0)
+                elif k=='percent_change_24h':
+                    price.append(float(v))
+        sorted_d = sorted(dict(zip(name,price)).items(), key=lambda x: x[1],reverse=True)
+        return sorted_d
+    except Exception as e:
+        print(e)
+
+def get_top_5():
+    top_5=[]
+    top_5 = get_price_all()[:6]
+    return top_5
+
+def get_bottom_5():
+    bottom_5 = []
+    bottom_5 = get_price_all()[-6:]
+    return bottom_5
