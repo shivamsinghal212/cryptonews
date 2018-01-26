ef575bda5816803f46c11ffbc5ef36b45a69488e
shivam <shivam.singhal212@gmail.com>
2018-01-26 20:55:12 +0530

    Added Convert cryptocurrency support

--- coinhacker/urls.py
+++ coinhacker/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('ico/',views.ico, name="ico"),
-    path('upcoming_ico/',views.upcoming_ico, name="upcoming_ico")
+    path('upcoming_ico/',views.upcoming_ico, name="upcoming_ico"),
+    path('convert/',views.get_rates, name="convert"),
+    path('converted/',views.get_rates)
 ]
